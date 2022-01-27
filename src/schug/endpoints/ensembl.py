6dc457306ac9cf935bf625de9e017aa97f38153e
Mei <25568561+projectoriented@users.noreply.github.com>
2022-01-27 15:21:52 +0100

    added doc string, type hint

--- src/schug/endpoints/ensembl.py
+++ src/schug/endpoints/ensembl.py
@@ -11,25 +11,25 @@ router = APIRouter()
 
 @router.get("/", response_model=List[EnsemblGeneRead])
 def read_ensembl_genes(
-        *,
         session: Session = Depends(get_session),
         limit: int = Query(default=100, lte=100),
-):
+) -> list[EnsemblGeneRead]:
+    """Get all ensembl genes from the database"""
     ensembl_genes = session.exec(select(EnsemblGene).limit(limit)).all()
     return ensembl_genes
 
 
 @router.get("/{ensembl_gene_id}", response_model=List[EnsemblTranscriptRead])
 def read_ensembl_transcripts(
-        *,
         ensembl_gene_id: str,
         session: Session = Depends(get_session),
         limit: int = Query(default=100, lte=100),
         only_canonical: Optional[bool] = False
-):
-    where_clause = select(EnsemblTranscript).where(EnsemblTranscript.ensembl_gene_id == ensembl_gene_id)
+) -> list[EnsemblTranscriptRead]:
+    """Get Ensembl transcripts with ensembl_gene_id."""
+    get_ensemble_transcripts = select(EnsemblTranscript).where(EnsemblTranscript.ensembl_gene_id == ensembl_gene_id)
     if only_canonical:
-        where_clause = where_clause.where(EnsemblTranscript.is_canonical)
+        get_ensemble_transcripts = get_ensemble_transcripts.where(EnsemblTranscript.is_canonical)
 
-    ensembl_transcripts = session.exec(where_clause.limit(limit)).all()
-    return ensembl_transcripts
+    get_ensemble_transcripts = session.exec(get_ensemble_transcripts.limit(limit)).all()
+    return get_ensemble_transcripts
