3f319254fdcaa02ce93c75379e5ac0ffb11c8321
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-02-03 18:17:01 +0100

    added exceptions to ensembl endpoints

--- src/schug/endpoints/ensembl.py
+++ src/schug/endpoints/ensembl.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query, status
+from fastapi import APIRouter, Query
+from .http_exceptions import SchugHttpException
 from schug.database.ensembl import get_ensembl_exons, get_ensembl_genes, get_ensembl_transcripts
 from schug.models import (
     EnsemblExonRead,
@@ -16,7 +17,9 @@ def read_ensembl_genes(
         limit: int = Query(default=100, lte=100),
 ) -> list[EnsemblGeneRead]:
     """Get all ensembl genes from the database"""
-    return get_ensembl_genes(limit)
+    genes: List[EnsemblGeneRead] = get_ensembl_genes(limit)
+    SchugHttpException.error_404(result=genes, query="Ensembl Genes")
+    return genes
 
 
 @router.get("/transcripts/{ensembl_gene_id}", response_model=List[EnsemblTranscriptRead])
@@ -26,7 +29,9 @@ def read_ensembl_transcripts(
         only_canonical: Optional[bool] = False
 ) -> list[EnsemblTranscriptRead]:
     """Get Ensembl transcripts with ensembl gene id."""
-    return get_ensembl_transcripts(ensembl_gene_id, only_canonical, limit)
+    result: List[EnsemblTranscriptRead] = get_ensembl_transcripts(ensembl_gene_id, only_canonical, limit)
+    SchugHttpException.error_404(result=result, query=ensembl_gene_id)
+    return result
 
 
 @router.get("/exons/{ensembl_transcript_id}", response_model=List[EnsemblExonRead])
@@ -34,4 +39,6 @@ def read_ensembl_exons(
         ensembl_transcript_id: str
 ) -> list[EnsemblExonRead]:
     """Get Ensembl exons with ensembl transcript id."""
-    return get_ensembl_exons(ensembl_transcript_id)
+    result: List[EnsemblExonRead] = get_ensembl_exons(ensembl_transcript_id)
+    SchugHttpException.error_404(result=result, query=ensembl_transcript_id)
+    return result
