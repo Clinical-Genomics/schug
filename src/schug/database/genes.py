10e8eb4b1a377d7a160ac76e72dc8716827accc8
Mei <25568561+projectoriented@users.noreply.github.com>
2022-03-20 23:53:32 +0100

    rm useless return

--- src/schug/database/genes.py
+++ src/schug/database/genes.py
@@ -3,7 +3,7 @@ from schug.database.session import get_session
 from schug.models.gene import EnsemblGene, Gene, into_gene
 
 
-def create_gene_item(ensembl_gene: EnsemblGene) -> Gene:
+def create_gene_item(ensembl_gene: EnsemblGene):
     """Insert Gene Entries"""
     converted = into_gene(ensembl_gene)
     db_entry = Gene.from_orm(converted)
@@ -12,4 +12,3 @@ def create_gene_item(ensembl_gene: EnsemblGene) -> Gene:
         session.add(db_entry)
         session.commit()
         session.refresh(db_entry)
-        return db_entry
