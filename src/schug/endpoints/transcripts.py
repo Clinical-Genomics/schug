4540deab3e082521be9927fd3aae6d851d8deb63
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-08 10:47:18 +0100

    Named arg

--- src/schug/endpoints/transcripts.py
+++ src/schug/endpoints/transcripts.py
@@ -41,7 +41,7 @@ def read_transcript_db_id(
 async def ensembl_transcripts(build: Build):
     """A proxy to the Ensembl Biomart that retrieves transcripts in a specific genome build."""
 
-    ensembl_client: EnsemblBiomartClient = fetch_ensembl_transcripts(build)
+    ensembl_client: EnsemblBiomartClient = fetch_ensembl_transcripts(build=build)
     url: str = ensembl_client.build_url(xml=ensembl_client.xml)
 
     return StreamingResponse(stream_resource(url), media_type="text/tsv")
