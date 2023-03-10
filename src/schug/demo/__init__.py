375f4e385fd4d761fcdb1d2429159157b3b8e562
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-10 14:06:54 +0100

    fix typo

--- src/schug/demo/__init__.py
+++ src/schug/demo/__init__.py
@@ -5,5 +5,5 @@ EXONS_37_FILE_PATH: str = importlib_resources.files("schug") / "demo/exons_37.ts
 EXONS_38_FILE_PATH: str = importlib_resources.files("schug") / "demo/exons_38.tsv"
 GENES_37_FILE_PATH: str = importlib_resources.files("schug") / "demo/genes_37.tsv"
 GENES_38_FILE_PATH: str = importlib_resources.files("schug") / "demo/genes_38.tsv"
-TRANSCRIPTS_37_FILE_PATH: importlib_resources.files("schug") / "demo/transcripts_37.tsv"
+TRANSCRIPTS_37_FILE_PATH: str = importlib_resources.files("schug") / "demo/transcripts_37.tsv"
 TRANSCRIPTS_38_FILE_PATH: str = importlib_resources.files("schug") / "demo/transcripts_38.tsv"
