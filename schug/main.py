4357782f972f1e8b6be56b47a46531ec055c357a
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-15 14:49:22 +0100

    Replace repeated string with constant

--- schug/main.py
+++ schug/main.py
@@ -4,6 +4,7 @@ from schug import __version__
 from .endpoints import exons, genes, transcripts
 
 app = FastAPI()
+NOT_FOUND_MESSAGE = "Not found"
 
 ### REST API
 
@@ -17,7 +18,7 @@ app.include_router(
     genes.router,
     prefix="/genes",
     tags=["genes"],
-    responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
+    responses={status.HTTP_404_NOT_FOUND: {"description": NOT_FOUND_MESSAGE}},
 )
 
 
@@ -25,12 +26,12 @@ app.include_router(
     transcripts.router,
     prefix="/transcripts",
     tags=["transcripts"],
-    responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
+    responses={status.HTTP_404_NOT_FOUND: {"description": NOT_FOUND_MESSAGE}},
 )
 
 app.include_router(
     exons.router,
     prefix="/exons",
     tags=["exons"],
-    responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
+    responses={status.HTTP_404_NOT_FOUND: {"description": NOT_FOUND_MESSAGE}},
 )
